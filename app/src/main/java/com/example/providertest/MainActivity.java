2e405aa03d9c0af70c194b1312b72fcdbaaa758f
zffjzwcfljj <1601950538@qq.com>
2019-11-12 14:02:25 +0800

    first commit

---
+++ app/src/main/java/com/example/providertest/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.providertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String newId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button add = (Button) findViewById(R.id.button);
+        Button query = (Button) findViewById(R.id.button2);
+        Button update = (Button) findViewById(R.id.button3);
+        Button delete = (Button) findViewById(R.id.button4);
+
+        add.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Uri uri = Uri.parse("content://com.example.wordlist10.provider/word");
+                ContentValues values = new ContentValues();
+                values.put("name","app");
+                values.put("Chinese","appp");
+                values.put("sentence","This is a app.");
+                Uri newUri = getContentResolver().insert(uri,values);
+                newId = newUri.getPathSegments().get(1);
+            }
+        });
+
+        query.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Uri uri = Uri.parse("content://com.example.wordlist10.provider/word");
+                Cursor cursor = getContentResolver().query(uri,null,null,null,null);
+                if (cursor != null){
+                    while (cursor.moveToNext()){
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String Chinese = cursor.getString(cursor.getColumnIndex("Chinese"));
+                        String sentence = cursor.getString(cursor.getColumnIndex("sentence"));
+                        Log.d("MainActivity","The word is"+name);
+                        Log.d("MainActivity","The meaning is"+Chinese);
+                        Log.d("MainActivity","The sample is"+sentence);
+                    }
+                    cursor.close();
+                }
+            }
+        });
+
+        update.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Uri uri = Uri.parse("content://com.example.wordlist10.provider/word/"+newId);
+                ContentValues values = new ContentValues();
+                values.put("name","appp");
+                values.put("Chinese","app");
+                values.put("sentence","This is a appp.");
+                getContentResolver().update(uri,values,null,null);
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Uri uri = Uri.parse("content://com.example.wordlist10.provider/word/"+newId);
+                getContentResolver().delete(uri,null,null);
+            }
+        });
+
+    }
+}
